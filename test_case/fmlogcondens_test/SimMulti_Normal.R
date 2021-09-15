61545a03349ad29ce10ad82d64bc1f317c2705b4
JaneeChoi <serimtech07@snu.ac.kr>
2021-09-15 21:06:21 +0900

    test results

--- test_case/fmlogcondens_test/SimMulti_Normal.R
+++ test_case/fmlogcondens_test/SimMulti_Normal.R
@@ -115,17 +115,21 @@ devtools::install_github("JaneeChoi/SpMix",ref = "fmlogcondens_merge")
 library(SpMix)
 set.seed(210828)
 
-M <- 1
+M<-1
 setwd("/Users/choiiiiii/Documents/GitHub/Semiparam_Mixture/test_case/fmlogcondens_test/2d_Normal_fmlogcondens")
+
+iternum=10
 for (N in c(100,500,1000,5000,10000,15000,20000,25000)){
-  time1<-system.time(Res.1<-SimMultNormal(M = M, n = N, p0 = 0.95))[3]
-  print("finish 1")
-  time2<-system.time(Res.2<-SimMultNormal(M = M, n = N, p0 = 0.90))[3]
-  print("finish 2")
-  time3<-system.time(Res.3<-SimMultNormal(M = M, n = N, p0 = 0.80))[3]
-  print("finish 3")
-  result_df<-data.frame(res1=c(time1,Res.1$p0hat.SP[1],Res.1$Sensitivity[1],Res.1$Specificity[1]),res2=c(time2,Res.2$p0hat.SP[1],Res.2$Sensitivity[1],Res.2$Specificity[1]),res3=c(time3,Res.3$p0hat.SP[1],Res.3$Sensitivity[1],Res.3$Specificity[1]))
-  write.csv(result_df,paste0("result_",N,".csv"))
+  time95=rep(0,iternum)
+  time90=rep(0,iternum)
+  time80=rep(0,iternum)
+  for (iter in 1:iternum){
+    time95[iter]<-system.time(Res.1<-SimMultNormal(M = M, n = N, p0 = 0.95))[3]
+    time90[iter]<-system.time(Res.3<-SimMultNormal(M = M, n = N, p0 = 0.90))[3]
+    time80[iter]<-system.time(Res.2<-SimMultNormal(M = M, n = N, p0 = 0.80))[3]
+  }
+  result_df<-data.frame(time95=time95,time90=time90,time85=time85)
+  write.csv(result_df,paste0("repeated_result_",N,".csv"))
   print(result_df)
   print("finish")
   print(N)
@@ -178,8 +182,8 @@ library(SpMix)
 set.seed(210828)
 
 M <- 1
-setwd("/Users/choiiiiii/Documents/GitHub/Semiparam_Mixture/test_case/fmlogcondens_test/3d_Gamma_fmlogcondens")
-for (N in c(100,500,1000,5000,10000,15000,20000,25000)){
+setwd("/Users/choiiiiii/Documents/GitHub/Semiparam_Mixture/test_case/fmlogcondens_test/3d_Gamma_LogConcDEAD")
+for (N in c(100,500,1000,5000)){
   time1<-system.time(Res.1<-SimMultGamma3d(M = M, n = N, p0 = 0.95))[3]
   print("finish 1")
   time2<-system.time(Res.2<-SimMultGamma3d(M = M, n = N, p0 = 0.90))[3]
